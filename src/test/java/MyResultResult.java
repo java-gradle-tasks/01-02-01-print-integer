445aa7364b84237a2a076bfe7c7ff45e8e761584
Gyuris Csaba <gycsaba72@gmail.com>
2022-02-11 11:57:44 +0100

    Update MyResultResult.java

--- src/test/java/MyResultResult.java
+++ src/test/java/MyResultResult.java
@@ -19,25 +19,16 @@
          PrintStream stdout = System.out;
          System.setOut(ps);
 
-         MyArea.main(new String[0]);
+         MyResult.main(new String[0]);
 
          System.setOut(stdout);
 
+         String expected="A havi eredmeny 1000 forint."+newLine;
          String actual=byteArrayOutputStream.toString();
-         
-         String expected="";
-         if (System.getProperty("os.name").startsWith("Windows")) {
-            expected = "A szoba kerulete 12,56 negyzetmeter." + newLine;
-         } else {
-            expected = "A szoba kerulete 12.56 negyzetmeter." + newLine;
-         }        
 
-         System.out.println(expected);
-         System.out.println(actual);
-         
          int result=expected.compareTo(actual);
 
-         Assertions.assertEquals(result,0,"A program kimenete nem a megfelelo karakterkebol epul fel!");
-         Assertions.assertEquals(expected,actual,"A program kimenete nem megfelelo!");
+         Assertions.assertEquals(result,0,"A program kimenete nem megfelelo!");
       }
    }
+
